dada34c806f79e38e6f709c1dd4d4a9f6c3cf2ee
Denis Zamosenchuk <denis.zamosenchuk@gmail.com>
2016-11-21 12:02:18 +0200

    extend immutable test with comparison of StringBuilder

--- home-work-3/src/TestStringImmutable.java
+++ home-work-3/src/TestStringImmutable.java
@@ -9,5 +9,12 @@ public class TestStringImmutable {
 
         System.out.printf("s1 : %s\n", s1);
         System.out.printf("s2 : %s\n", s2);
+
+        StringBuilder s3 = new StringBuilder("string 3");
+        StringBuilder s4 = s3;
+        s3.append(" add word");
+
+        System.out.printf("s3 : %s\n", s3);
+        System.out.printf("s4 : %s\n", s4);
     }
 }
