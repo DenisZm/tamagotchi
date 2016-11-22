e07e0d00af08faeb1358718c6c7fb3b8fc8665f5
Denis Zamosenchuk <denis.zamosenchuk@gmail.com>
2016-11-22 09:49:01 +0200

    make cycles int Test classes - it works :)

--- home-work-3/src/TestString.java
+++ home-work-3/src/TestString.java
@@ -2,14 +2,13 @@
  * Created by Denis on 17.11.2016.
  */
 public class TestString implements Test {
-    @Override
 
-    public void printString() {
-        String str1 = "Initial test string";
-        str1 += " add some words";
-        str1 += " and another word";
-        str1 += " more more words" + " test append";
-        str1 += " springs";
-        System.out.println(str1);
+    @Override
+    public void createString(String sample, int count ) {
+        String resultString = new String();
+        for (int i = 0; i < count; i++) {
+            resultString += sample;
+        }
+        // return resultString;
     }
 }
